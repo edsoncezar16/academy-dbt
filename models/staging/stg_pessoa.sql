cdb11bf5ce5ca84cecfd5285e472a79ca3f4bdd6
Edson Nogueira <edson.cezar@indicium.tech>
2023-12-29 00:55:02 +0000

    Fix null values in customers full name.

--- models/staging/stg_pessoa.sql
+++ models/staging/stg_pessoa.sql
@@ -2,7 +2,12 @@ with
     source_data as (
         select
             businessentityId as ID_ENTIDADE_NEGOCIO
-            , concat(firstname, ' ', middlename, ' ', lastname) as NOME_COMPLETO
+            , concat(
+                coalesce(title, ''), ' '
+                , coalesce(firstname, ''), ' '
+                , coalesce(lastname, ''), ' '
+                , coalesce(suffix, '')
+                ) as NOME_COMPLETO
         from {{source('indicium-ae-certification','person')}}
 )
 
