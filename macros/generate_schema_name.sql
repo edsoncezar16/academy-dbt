f748a4f30ffdbdbfb7999d1c0c94233e9bc95745
Edson Nogueira <edson.cezar@indicium.tech>
2023-12-31 20:12:11 +0000

    Improve condition on custom schema name.

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,21 +1,13 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if target.name in ['prod', 'dev'] and custom_schema_name is not none -%}
+    {%- if custom_schema_name is not none -%}
 
         {{ custom_schema_name | trim }}
 
     {%- else -%}
     
-        {%- if custom_schema_name is none -%}
-
-            {{ default_schema }}
-        
-        {%- else -%}
-
-            {{ custom_schema_name | trim }}
-
-        {%- endif -%}
+        {{ default_schema }}
     
     {%- endif -%}
 
