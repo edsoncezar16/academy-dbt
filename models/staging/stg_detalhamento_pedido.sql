ddebae39a6a50948bbdd999c405198d46495c85e
edson.cezar@indicium.tech <edson.cezar@indicium.tech>
2024-01-23 22:57:50 +0000

    Fix float64 casting.

--- models/staging/stg_detalhamento_pedido.sql
+++ models/staging/stg_detalhamento_pedido.sql
@@ -4,8 +4,8 @@ with
             cast(salesorderid as int) as ID_VENDA
             , cast(productid as int) as ID_PRODUTO
             , cast(orderqty as int) as QUANTIDADE_COMPRADA
-            , cast(unitprice as float) as PRECO_UNITARIO
-            , cast(unitpricediscount as float) as DESCONTO_PERCENTUAL_UNITARIO
+            , cast(unitprice as float64) as PRECO_UNITARIO
+            , cast(unitpricediscount as float64) as DESCONTO_PERCENTUAL_UNITARIO
         from {{source('indicium-ae-certification','salesorderdetail')}}
 )
 
