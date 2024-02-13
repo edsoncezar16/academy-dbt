36d822b18a0c91afdf41c1c31744864609341708
Edson Nogueira <edsoncezar16@gmail.com>
2024-02-13 19:34:14 -0300

    Add meltano assets to code location.

--- academy_dbt_dagster/academy_dbt_dagster/definitions.py
+++ academy_dbt_dagster/academy_dbt_dagster/definitions.py
@@ -12,7 +12,7 @@ from .meltano_dagster_utils import meltano_dagster_factory
 meltano_assets, meltano_jobs, meltano_schedules = meltano_dagster_factory()
 
 defs = Definitions(
-    assets=[indicium_ae_certification_dbt_assets],
+    assets=[indicium_ae_certification_dbt_assets] + meltano_assets,
     schedules=dbt_schedules + meltano_schedules,
     resources={
         "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
