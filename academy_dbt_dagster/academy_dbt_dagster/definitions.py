2542c7f46456cc1ef0756f8e8f432a5d0a199d49
Edson Nogueira <edsoncezar16@gmail.com>
2024-02-14 00:26:34 -0300

    Integrates meltano streams as dagster assets.

--- academy_dbt_dagster/academy_dbt_dagster/definitions.py
+++ academy_dbt_dagster/academy_dbt_dagster/definitions.py
@@ -5,17 +5,19 @@ from dagster_dbt import DbtCliResource
 from dagster_meltano import MeltanoResource
 
 from .assets import indicium_ae_certification_dbt_assets
-from .constants import dbt_project_dir, meltano_project_dir
+from .constants import dbt_project_dir
 from .schedules import dbt_schedules
 from .meltano_dagster_utils import meltano_dagster_factory
+from .constants import meltano_project_dir
 
-meltano_assets, meltano_jobs, meltano_schedules = meltano_dagster_factory()
+meltano_assets, meltano_jobs, meltano_schedules = meltano_dagster_factory(
+    MeltanoResource(project_dir=meltano_project_dir)
+)
 
 defs = Definitions(
     assets=[indicium_ae_certification_dbt_assets] + meltano_assets,
     schedules=dbt_schedules + meltano_schedules,
     resources={
         "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
-        "meltano": MeltanoResource(project_dir=meltano_project_dir),
     },
 )
--- academy_dbt_dagster/academy_dbt_dagster/meltano_dagster_utils.py
+++ academy_dbt_dagster/academy_dbt_dagster/meltano_dagster_utils.py
@@ -1,34 +1,37 @@
 import yaml
+import asyncio
 
 from dagster import (
     ScheduleDefinition,
     DefaultScheduleStatus,
     AssetOut,
     OpExecutionContext,
-    ConfigurableResource,
     AssetSelection,
     define_asset_job,
     multi_asset,
 )
 from dagster_meltano import MeltanoResource
+from dagster_meltano.utils import generate_dagster_name
 from typing import Mapping, Any
+from .constants import meltano_project_dir
 
 
-
-
-def get_catalog(plugin_name: str, meltano: MeltanoResource) -> list[Mapping[str, Any]]:
+def get_catalog(tap_name: str, meltano: MeltanoResource) -> list[Mapping[str, Any]]:
     """
-    Returns the catalog of all streams for a given plugin from the Meltano project.
+    Returns the catalog of all streams for a given tap from the Meltano project.
+    tap_name is the name after the 'tap-' prefix, e.g, for tap-postgres we have tap_name='postgres'.
     """
     try:
-        catalog = meltano.load_json_from_cli(["invoke", "--dump=catalog", plugin_name])
+        catalog = asyncio.run(
+            meltano.load_json_from_cli(["invoke", "--dump=catalog", f"tap-{tap_name}"])
+        )
         return catalog.get("streams", [])
     except ValueError as e:
-        print(f"Error generating catalog for plugin '{plugin_name}': {e.stderr}")
+        print(f"Error generating catalog for plugin 'tap-{tap_name}'. {e}")
         return []
 
 
-def get_selected_streams(meltano: MeltanoResource) -> Mapping[str, list[str]]:
+def get_selected_streams(meltano: MeltanoResource) -> dict[str, list[str]]:
     """
     Generates a mapping of a plugin to its selected streams across all plugins in the Meltano project.
     """
@@ -36,16 +39,19 @@ def get_selected_streams(meltano: MeltanoResource) -> Mapping[str, list[str]]:
         meltano_config = yaml.safe_load(file)
 
     plugins = meltano_config.get("plugins", {})
+    taps = plugins.get("extractors", {})
     selected_streams = {}
 
-    for plugin_type, plugin_list in plugins.items():
-        for plugin in plugin_list:
-            plugin_name = plugin.get("name")[4:] # removes the 'tap-' prefix
-            if plugin_name:
-                catalog = get_catalog(plugin_name)
-                selected_streams[plugin_name] = [
-                    stream for stream in catalog if stream.get("selected")
-                ]
+    for tap in taps:
+        full_tap_name = tap.get("name")
+        if full_tap_name:
+            tap_name = full_tap_name[4:]  # removes the 'tap-' prefix
+            catalog = get_catalog(tap_name, meltano)
+            selected_streams[tap_name] = [
+                stream.get("tap_stream_id")
+                for stream in catalog
+                if stream.get("selected")
+            ]
 
     return selected_streams
 
@@ -60,30 +66,36 @@ def meltano_dagster_factory(meltano: MeltanoResource) -> list[Any]:
     schedules = []
 
     for tap_name, tap_streams in get_selected_streams(meltano).items():
+
         @multi_asset(
-            name=tap_name,
-            resource_defs={'meltano': MeltanoResource},
+            name=generate_dagster_name(tap_name),
             compute_kind="meltano",
-            group_name=tap_name,
+            resource_defs={"meltano": MeltanoResource(project_dir=meltano_project_dir)},
+            group_name=generate_dagster_name(tap_name),
             outs={
-            stream: AssetOut()
-            for stream
-            in tap_streams
-            }
+                generate_dagster_name(stream_name): AssetOut(
+                    key=generate_dagster_name(stream_name)
+                )
+                for stream_name in tap_streams
+            },
         )
-        def compute(context: OpExecutionContext, meltano: MeltanoResource):
+        def compute(context: OpExecutionContext):
             command = f"run tap-{context.op.name} target-postgres"
-            meltano.execute_command(f"{command}", dict(), context.log)
-            return tuple([None for _ in context.selected_output_names])
-        
+            meltano = context.resources.get("meltano")
+            if meltano:
+                meltano.execute_command(f"{command}", dict(), context.log)
+                return tuple([None for _ in context.selected_output_names])
+
         multi_assets.append(compute)
 
-        asset_job = define_asset_job(f"{tap_name}_assets", AssetSelection.groups(tap_name))
+        asset_job = define_asset_job(
+            f"{tap_name}_assets", AssetSelection.groups(tap_name)
+        )
 
         basic_schedule = ScheduleDefinition(
-            job=asset_job, 
-            cron_schedule="0 0 * * 1-5", 
-            default_status=DefaultScheduleStatus.RUNNING
+            job=asset_job,
+            cron_schedule="0 0 * * 1-5",
+            default_status=DefaultScheduleStatus.RUNNING,
         )
 
         jobs.append(asset_job)
